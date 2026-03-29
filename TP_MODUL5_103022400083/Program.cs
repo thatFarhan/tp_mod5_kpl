271a04884a6f45d5cbed21145d6be4201cd24ed5
thatFarhan <fabdillahmu@gmail.com>
2026-03-29 17:55:28 +0700

    menambahkan class DataGeneric

--- TP_MODUL5_103022400083/Program.cs
+++ TP_MODUL5_103022400083/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿DataGeneric<long> nim = new DataGeneric<long>(103022400083);
+nim.PrintData();
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
